53e352f2b0550738643de37261f7488a61500636
Abhay Pratap <abhaypratapsingh3008@gmail.com>
2025-05-15 23:05:27 +0530

    working with files

--- files/files.go
+++ files/files.go
@@ -11,5 +11,8 @@ func main() {
 	file, _ := os.Create("./file.txt")
 	io.WriteString(file, "Hello World!")
 	fmt.Println("file has been created")
+	file.Close()
+	readfile, _ := os.ReadFile("./file.txt")
+	fmt.Println("reading file: ", string(readfile))
 
 }
