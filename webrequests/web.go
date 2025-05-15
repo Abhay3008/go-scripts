6a07e45528dd8bcbb026404b019b465d64071bd3
Abhay Pratap <abhaypratapsingh3008@gmail.com>
2025-05-15 23:05:18 +0530

    get/post request

--- webrequests/web.go
+++ webrequests/web.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	uri := "https://reqres.in/api/users"
+	// GET request
+	getRequest(uri)
+
+	// POST request
+	PostRequest(uri)
+}
+func getRequest(uri string) {
 	req, err := http.NewRequest("GET", uri, nil)
 	if err != nil {
 		panic(err)
@@ -17,7 +25,21 @@ func main() {
 	resp, _ := client.Do(req)
 	response, _ := io.ReadAll(resp.Body)
 	defer resp.Body.Close()
-
 	fmt.Println(string(response))
-
+}
+func PostRequest(uri string) {
+	body := `{
+			"name": "morpheus",
+			"job": "leader"
+		}`
+	r := strings.NewReader(body)
+	req, err := http.NewRequest("POST", uri, r)
+	req.Header.Add("x-api-key", "reqres-free-v1")
+	if err != nil {
+		panic(err)
+	}
+	client := &http.Client{}
+	resp, _ := client.Do(req)
+	response, _ := io.ReadAll(resp.Body)
+	fmt.Println(string(response))
 }
